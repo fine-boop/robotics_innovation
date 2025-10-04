a77dfd4e5ea556b8a1670a5f8520b7e96efce456
julian banim <julianb@localhost.localdomain>
2025-10-04 11:46:28 +0100

    the beginningp 2

---
+++ local/photo_uploading/app.py
@@ -0,0 +1,8 @@
+# what we want to do: feed images into a function to convert them to photogrammerty 3d model, and store it in a website.
+# course of action :
+# user log in/out website, code something that reads all images from a directory, zips them and uploads to server. 
+#i actually think bash would be easier for this lol. we can still use python for automation.
+# this file should control the cameras and save the images to a directory, photos 
+#next it should call the bash script to zip and upload the images to the server.
+# it will be running hand in hand with the local website to control the cameras and upload the images.
+
