b29f4c1493b61b82c3bb752ca06f555ca4f4d02d
h4z3 <145603285+fine-boop@users.noreply.github.com>
2025-12-09 19:11:45 +0000

    JEllo

--- v1-finished/localsite/julians_version/app2.py
+++ v1-finished/localsite/julians_version/app2.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import time
 import secrets
 import sqlite3
 
 app = Flask(__name__)
-
+app.secret_key = secrets.token_hex(16)
 
 def init_db():
     conn = sqlite3.connect('database.db')
@@ -14,29 +14,24 @@ def init_db():
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    email TEXT UNIQUE NOT NULL,
                    username TEXT NOT NULL,
-                   password TEXT NOT NULL)
-    """)
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS sites(
+                   password TEXT NOT NULL)""")
+    
+    cursor.execute("""CREATE TABLE IF NOT EXISTS sites(
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    owner_id INTEGER NOT NULL,
-                   FOREGIN KEY (owner_id) REFERENCES users(id),
-                   )
-    """)
+                   FOREIGN KEY (owner_id) REFERENCES users(id))""")
     cursor.execute("""CREATE TABLE IF NOT EXISTS site_members(
                    site_id INTEGER NOT NULL,
                    user_id INTEGER NOT NULL,
                    PRIMARY KEY (site_id, user_id),
-                   FOREGIN KEY (site_id) REFERENCES sites(id),
-                   FOREGIN KEY (user_id) REFERENCES users(id)
-                   """)
+                   FOREIGN KEY (site_id) REFERENCES sites(id),
+                   FOREIGN KEY (user_id) REFERENCES users(id))""")
     
     cursor.execute("""CREATE TABLE IF NOT EXISTS artefacts(
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    name TEXT UNIQUE,
                    amount INTEGER,
-                   weight FLOAT,
-                   )
+                   weight FLOAT)
     """)
 
 def connect_db():
@@ -57,3 +52,35 @@ def signup():
         provided_username = request.form['provided_username']
         provided_email = request.form['provided_email']
         provided_password = request.form['provided_password']
+        row_exists = cursor.execute("SELECT 1 FROM users WHERE email = ? LIMIT 1", (provided_email,)).fetchone()
+        if row_exists:
+            flash("Account Exists")
+            return redirect('/signup')
+        else:
+            cursor.execute("""INSERT INTO users(email, username, password) VALUES(?, ?, ?)""", (provided_email, provided_username, provided_password))
+            conn.commit()
+            conn.close()
+            flash("Signup successful!")
+            return redirect('/signup')
+    return render_template('signup.html')
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    if request.method == "POST" and "login" in request.form:
+        conn, cursor = connect_db()
+        provided_username = request.form['username']
+        provided_email = request.form['email']
+        provided_password = request.form['password']
+        row_exists = cursor.execute("SELECT 1 FROM users WHERE email = ? LIMIT 1", (provided_email,)).fetchone()
+        if row_exists:
+            flash("Account Exists")
+            return redirect('/signup')
+        else:
+            
+
+
+
+
+if __name__ == "__main__":
+    init_db()
+    app.run()
