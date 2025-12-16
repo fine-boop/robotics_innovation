63ed01f3d2a01c26974e17fa5639d7719e9a92f8
h4z3 <145603285+fine-boop@users.noreply.github.com>
2025-12-16 16:35:23 +0000

    update_login

--- v1-finished/localsite/julians_version/app2.py
+++ v1-finished/localsite/julians_version/app2.py
@@ -1,14 +1,22 @@
+#define the imports
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import time
 import secrets
 import sqlite3
 
+
+#initialize the app and secret key
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
+
+#initialize the database
 def init_db():
+    #create the datasbase, or connect if it exists
     conn = sqlite3.connect('database.db')
+    #open a cursor to perform db operations
     cursor = conn.cursor()
+    #create the tables
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users(
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,24 +40,30 @@ def init_db():
                    weight FLOAT)
     """)
 
+#this func exists to connect to the database and open a cursor 
 def connect_db():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     return conn, cursor
     
 
-
+#define the what happens when the users hits the / route (main page)
 @app.route('/', methods = ["GET", "POST"])
 def home():
     return render_template('home.html')
 
+#same for signup 
 @app.route('/signup', methods = ["GET", "POST"])
 def signup():
+    #if the user is sending data (post req) 
     if request.method == "POST" and "signup" in request.form:
+        #connect to the db with the function i made earlier
         conn, cursor = connect_db()
+        #define the variables from the form
         provided_username = request.form['provided_username']
         provided_email = request.form['provided_email']
         provided_password = request.form['provided_password']
+        #check if the email already exists (so theres not duplicate accounts)
         row_exists = cursor.execute("SELECT 1 FROM users WHERE email = ? LIMIT 1", (provided_email,)).fetchone()
         if row_exists:
             flash("Account Exists")
@@ -71,14 +85,18 @@ def login():
         provided_password = request.form['password']
         row_exists = cursor.execute("SELECT 1 FROM users WHERE email = ? LIMIT 1", (provided_email,)).fetchone()
         if row_exists:
-            flash("Account Exists")
-            return redirect('/signup')
+            row = cursor.execute("SELECT password FROM users WHERE email = ?", (provided_email,)).fetchone()
+            db_pass = row[-1]
+            print(db_pass)
+            exit() 
+
         else:
-            
+            pass
+    return render_template('login.html')
 
 
 
 
 if __name__ == "__main__":
     init_db()
-    app.run()
+    app.run(debug=True)
